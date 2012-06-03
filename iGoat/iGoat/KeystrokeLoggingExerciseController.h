5a04b3806667b280c68ccc7af6580362314aca24
Ken van Wyk <krvw@owasp.org>
2012-06-03 13:34:56 -0400

    Actually added the new files this time...

---
+++ iGoat/iGoat/KeystrokeLoggingExerciseController.h
@@ -0,0 +1,16 @@
+//
+//  KeystrokeLoggingExerciseController.h
+//  iGoat
+//
+//  Created by rutger_i on 5/23/12.
+//  Copyright (c) 2012 KRvW Associates, LLC. All rights reserved.
+//
+
+#import "ExerciseViewController.h"
+
+@interface KeystrokeLoggingExerciseController : ExerciseViewController
+
+@property (nonatomic, retain) IBOutlet UITextField *subjectField;
+@property (nonatomic, retain) IBOutlet UITextField *messageField;
+
+@end
